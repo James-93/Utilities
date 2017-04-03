29e9c2599d36ea7d97eb4a7c874cd220b6dd86c7
James-93 <jamesrandle1024@gmail.com>
2017-04-03 10:42:18 +0100

    Update MappingObjects.cs
    Swapping method to an extension method and removing manual threading optimisation as Parallel for each does this anyway.

--- Utilities/MappingObjects.cs
+++ Utilities/MappingObjects.cs
@@ -8,34 +8,21 @@ namespace Utilities
 {
     public class MappingObjects
     {
-        public List<object> MapChildren(IEnumerable<object> Parents, string ChildListAtributeName, IEnumerable<object> Children, string MatchingField, bool DefaultParentsToEmptyList = true)
+        public static void MapChildren(this IEnumerable<object> Parents, IEnumerable<object> Children, string MatchingField, string ChildListAtributeName)
         {
             if (Parents.Count() > 0 && Children.Count() > 0)
             {
                 if (Parents.Select(x => x.GetType().GetProperty(MatchingField)).All(x => x != null) && Children.Select(x => x.GetType().GetProperty(MatchingField)).All(x => x != null)) {
-                    if (Parents.Count() > 10000)
+                    Parallel.ForEach(Parents, parent =>
                     {
-                        Parallel.ForEach(Parents, parent =>
-                        {
-                            var parentValue = parent.GetType().GetProperty(MatchingField).GetValue(parent, null);
-                            var parentChildren = Children.Where(x => x.GetType().GetProperty(MatchingField).GetValue(x, null) == parentValue);
-                            parent.GetType().GetProperty(ChildListAtributeName).SetValue(parent, parentChildren);
-                        });
-                    }
-                    else
-                    {
-                        foreach (var parent in Parents)
-                        {
-                            var parentValue = parent.GetType().GetProperty(MatchingField).GetValue(parent, null);
-                            var parentChildren = Children.Where(x => x.GetType().GetProperty(MatchingField).GetValue(x, null) == parentValue);
-                            parent.GetType().GetProperty(ChildListAtributeName).SetValue(parent, parentChildren);
-                        }
-                    }
+                        var parentValue = parent.GetType().GetProperty(MatchingField).GetValue(parent, null);
+                        var parentChildren = Children.Where(x => x.GetType().GetProperty(MatchingField).GetValue(x, null) == parentValue);
+                        parent.GetType().GetProperty(ChildListAtributeName).SetValue(parent, parentChildren);
+                    });
                 }
                 else
                     throw new Exception($"Missing Field '{MatchingField}' does not exist in all list elements");
             }
-            return Parents.ToList();
         }
     }
 }
