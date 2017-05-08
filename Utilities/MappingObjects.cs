da3b5b2324f4839dadce026fdc778e33a1a83dd4
James-93 <jamesrandle1024@gmail.com>
2017-05-08 20:01:27 +0100

    Committing new iteration.

--- Utilities/MappingObjects.cs
+++ Utilities/MappingObjects.cs
@@ -2,28 +2,47 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Utilities.Mapping
 {
-    public static class MappingChildren
+  public static class MappingChildren
+  {
+    public static void MapChildren<T, N>(this IEnumerable<T> Parents, IEnumerable<N> Children, string MatchingField)
     {
-        public static void MapChildren<T, N>(this IEnumerable<T> Parents, IEnumerable<N> Children, string MatchingField, string ChildListAtributeName)
+      if (Parents.Count() > 0 && Children.Count() > 0)
+      {
+        if (Parents.Select(x => x.GetType().GetProperty(MatchingField)).All(x => x != null) && Children.Select(x => x.GetType().GetProperty(MatchingField)).All(x => x != null))
         {
-            if (Parents.Count() > 0 && Children.Count() > 0)
+          string listPattern = $"List..\\[{Children.FirstOrDefault().GetType().ToString()}\\]";
+          var r = new Regex(listPattern, RegexOptions.IgnoreCase);
+
+          string ChildListAtributeName = Parents.FirstOrDefault().GetType().GetProperties().Where(x => r.Match(x.PropertyType.ToString()).Success).Select(x => x.Name).FirstOrDefault();
+
+          if (ChildListAtributeName == null)
+          {
+            string ienumerablePattern = $"IEnumerable..\\[{Children.FirstOrDefault().GetType().ToString()}\\]";
+            r = new Regex(ienumerablePattern, RegexOptions.IgnoreCase);
+
+            ChildListAtributeName = Parents.FirstOrDefault().GetType().GetProperties().Where(x => r.Match(x.PropertyType.ToString()).Success).Select(x => x.Name).FirstOrDefault();
+          }
+
+          if (ChildListAtributeName != null)
+          {
+            Parallel.ForEach(Parents, parent =>
             {
-                if (Parents.Select(x => x.GetType().GetProperty(MatchingField)).All(x => x != null) && Children.Select(x => x.GetType().GetProperty(MatchingField)).All(x => x != null))
-                {
-                    foreach (var parent in Parents)
-                    {
-                        var parentValue = parent.GetType().GetProperty(MatchingField).GetValue(parent, null);
-                        var parentChildren = Children.Where(x => x.GetType().GetProperty(MatchingField).GetValue(x, null).ToString() == parentValue.ToString()).Select(x => (N)x);
-                        parent.GetType().GetProperty(ChildListAtributeName).SetValue(parent, parentChildren);
-                    }
-                }
-                else
-                    throw new Exception($"Missing Field '{MatchingField}' does not exist in all list elements");
-            }
+              var parentValue = parent.GetType().GetProperty(MatchingField).GetValue(parent, null);
+              var parentChildren = Children.Where(x => x.GetType().GetProperty(MatchingField).GetValue(x, null).ToString() == parentValue.ToString()).Select(x => (N)x);
+              parent.GetType().GetProperty(ChildListAtributeName).SetValue(parent, parentChildren.ToList());
+            });
+          }
+          else
+            throw new Exception($"Parent object does not contain List or IEnumerable of child object");
         }
+        else
+          throw new Exception($"Missing Field '{MatchingField}' does not exist in all list elements");
+      }
     }
+  }
 }
